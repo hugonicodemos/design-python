37586cf4845e4e8273de5bc690e2b4186c18c002
Hugo Nicodemos <hugonicodemos@gmail.com>
2015-01-13 22:15:27 -0300

    First test case created

--- poker.py
+++ poker.py
@@ -7,7 +7,13 @@ def hand_rank(hand):
 
 def test():
     """ Test cases for the functions in poker program. """
+    sf = "6C 7C 8C 9C TC".split()
+    fk = "9D 9H 9S 9C 7D".split()
+    fh = "TD TC TH 7C 7D".split()
+    assert poker([sf, fk, fh]) == sf
+    return "tests pass"
 
+print test()
 
 
 print max([3, 4, 5, 0]), max([3, 4, -5, 0], key=abs)
